e5152fc5547042f6d6f0966ccead5e02b0cb9417
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-03-10 11:44:38 +0000

    add login function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -28,6 +29,21 @@ class AuthController extends Controller
         return response()->json([
             'user' => $user,
             'token' => $token,
+        ]);
+    }
+
+    public function login(Request $request) {
+        if (!Auth::attempt($request->only('email','password'))) {
+            return response()->json([
+                'message' => 'Invalide credentials'
+            ],401);
+        }
+
+        $token = $request->user()->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $request->user ,
+            'token' => $token
         ]);
     }
 }
